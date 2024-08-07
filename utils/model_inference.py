b41060e46fc4ed4cb05024c782f49444452738d0
mori8 <kaithape@gmail.com>
2024-08-07 15:45:57 +0900

    Update: generate inference inputs and modify .gitignore

--- utils/model_inference.py
+++ utils/model_inference.py
@@ -22,17 +22,6 @@ from utils.exceptions import ModelInferenceException, wrap_exceptions
 from utils.save_file import upload_to_s3
 from utils.huggingface_api import (HUGGINGFACE_INFERENCE_API_URL, get_hf_headers)
 from utils.model_selection import Model
-from utils.resources import (
-    audio_from_bytes,
-    encode_audio,
-    encode_image,
-    get_prompt_resource,
-    get_resource_url,
-    image_from_bytes,
-    load_image,
-    save_audio,
-    save_image,
-)
 from utils.task_parsing import Task
 
 logger = logging.getLogger(__name__)
@@ -52,11 +41,9 @@ def infer_huggingface(task: str, model_id: str, input_args: Any, session: reques
                 endpoint = model_data["endpoint"]
                 break
     
-    huggingface_task = create_huggingface_task(task=task, model_id=model_id)
-    data = {
-        "inputs": input_args
-    }
-    print(data)
+    huggingface_task = create_huggingface_task(task=task, model_id=model_id, input_args=input_args)
+    data = huggingface_task.inference_inputs
+    # print(data)
     headers = get_hf_headers()
     # print(headers)
     try:
@@ -99,352 +86,26 @@ def get_model_input_format(model_id):
 
 # NLP Tasks
 
-
-# deepset/roberta-base-squad2 was removed from huggingface_models-metadata.jsonl because it is currently broken
-# Example added to task-planning-examples.json compared to original paper
-class QuestionAnswering:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        data = {
-            "inputs": {
-                "question": self.task.args["question"],
-                "context": self.task.args["context"]
-                if "context" in self.task.args
-                else "",
-            }
-        }
-        return json.dumps(data)
-
-    def parse_response(self, response):
-        return response.json()
-
-
-# Example added to task-planning-examples.json compared to original paper
-class SentenceSimilarity:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        data = {
-            "inputs": {
-                "source_sentence": self.task.args["text1"],
-                "sentences": [self.task.args["text2"]],
-            }
-        }
-        # Using string to bypass requests' form encoding
-        return json.dumps(data)
-
-    def parse_response(self, response):
-        return response.json()
-
-
-# Example added to task-planning-examples.json compared to original paper
-class TextClassification:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        return self.task.args["text"]
-        # return {"inputs": self.task.args["text"]}
-
-    def parse_response(self, response):
-        return response.json()
-
-
-class TokenClassification:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        return self.task.args["text"]
-
-    def parse_response(self, response):
-        return response.json()
-
-
-# CV Tasks
-class VisualQuestionAnswering:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        img_data = encode_image(self.task.args["image"])
-        img_base64 = base64.b64encode(img_data).decode("utf-8")
-        data = {
-            "inputs": {
-                "question": self.task.args["text"],
-                "image": img_base64,
-            }
-        }
-        return json.dumps(data)
-
-    def parse_response(self, response):
-        return response.json()
-
-
-class DocumentQuestionAnswering:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        img_data = encode_image(self.task.args["image"])
-        img_base64 = base64.b64encode(img_data).decode("utf-8")
-        data = {
-            "inputs": {
-                "question": self.task.args["text"],
-                "image": img_base64,
-            }
-        }
-        return json.dumps(data)
-
-    def parse_response(self, response):
-        return response.json()
-
-
-class TextToImage:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        return self.task.args["text"]
-
-    def parse_response(self, response):
-        image = image_from_bytes(response.content)
-        path = save_image(image)
-        return {"generated image": path}
-
-
-class ImageSegmentation:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        return encode_image(self.task.args["image"])
-
-    def parse_response(self, response):
-        image_url = get_resource_url(self.task.args["image"])
-        image = load_image(image_url)
-        colors = []
-        for i in range(len(response.json())):
-            colors.append(
-                (
-                    random.randint(100, 255),
-                    random.randint(100, 255),
-                    random.randint(100, 255),
-                    55,
-                )
-            )
-        predicted_results = []
-        for i, pred in enumerate(response.json()):
-            mask = pred.pop("mask").encode("utf-8")
-            mask = base64.b64decode(mask)
-            mask = Image.open(BytesIO(mask), mode="r")
-            mask = mask.convert("L")
-
-            layer = Image.new("RGBA", mask.size, colors[i])
-            image.paste(layer, (0, 0), mask)
-            predicted_results.append(pred)
-        path = save_image(image)
-        return {
-            "generated image with segmentation mask": path,
-            "predicted": predicted_results,
-        }
-
-
-# Not yet implemented in huggingface inference API
-class ImageToImage:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        img_data = encode_image(self.task.args["image"])
-        img_base64 = base64.b64encode(img_data).decode("utf-8")
-        data = {
-            "inputs": {
-                "image": img_base64,
-            }
-        }
-        if "text" in self.task.args:
-            data["inputs"]["prompt"] = self.task.args["text"]
-        return json.dumps(data)
-
-    def parse_response(self, response):
-        image = image_from_bytes(response.content)
-        path = save_image(image)
-        return {"generated image": path}
-
-
-class ObjectDetection:
-    def __init__(self, task: Task):
-        self.task = task
-
-    @property
-    def inference_inputs(self):
-        return encode_image(self.task.args["image"])
-
-    def parse_response(self, response):
-        image_url = get_resource_url(self.task.args["image"])
-        image = load_image(image_url)
-        draw = ImageDraw.Draw(image)
-        labels = list(item["label"] for item in response.json())
-        color_map = {}
-        for label in labels:
-            if label not in color_map:
-                color_map[label] = (
-                    random.randint(0, 255),
-                    random.randint(0, 100),
-                    random.randint(0, 255),
-                )
-        for item in response.json():
-            box = item["box"]
-            draw.rectangle(
-                ((box["xmin"], box["ymin"]), (box["xmax"], box["ymax"])),
-                outline=color_map[item["label"]],
-                width=2,
-            )
-            draw.text(
-                (box["xmin"] + 5, box["ymin"] - 15),
-                item["label"],
-                fill=color_map[item["label"]],
-            )
-        path = save_image(image)
-        return {
-            "generated image with predicted box": path,
-            "predicted": response.json(),
-        }
-
-
-# Example added to task-planning-examples.json compared to original paper
-class ImageClassification:
-    def __init__(self, task: Task, model_id: str):
-        self.task = task
-        self.model_id = model_id
-
-    @property
-    def inference_inputs(self):
-        return encode_image(self.task.args["image"])
-
-    def parse_response(self, response):
-        return response.json()
-
-
-class ImageToText:
-    def __init__(self, task: Task, model_id: str):
-        self.task = task
-        self.model_id = model_id
-
-    @property
-    def inference_inputs(self):
-        return encode_image(self.task.args["image"])
-
-    def parse_response(self, response):
-        return {"generated text": response.json()[0].get("generated_text", "")}
-
-
-# Audio Tasks
-class TextToSpeech:
-    def __init__(self, task: Task, model_id: str):
-        self.task = task
-        self.model_id = model_id
-
-    @property
-    def inference_inputs(self):
-        return self.task.args["text"]
-
-    def parse_response(self, response):
-        audio = audio_from_bytes(response.content)
-        path = save_audio(audio)
-        return {"generated audio": path}
-
-
-class AudioToAudio:
-    def __init__(self, task: Task, model_id: str):
-        self.task = task
-        self.model_id = model_id
-
-    @property
-    def inference_inputs(self):
-        return encode_audio(self.task.args["audio"])
-
-    def parse_response(self, response):
-        result = response.json()
-        blob = result[0].items()["blob"]
-        content = base64.b64decode(blob.encode("utf-8"))
-        audio = audio_from_bytes(content)
-        path = save_audio(audio)
-        return {"generated audio": path}
-
-
-class AutomaticSpeechRecognition:
-    def __init__(self, task: Task, model_id: str):
-        self.task = task
-        self.model_id = model_id
-
-    @property
-    def inference_inputs(self):
-        return encode_audio(self.task.args["audio"])
-
-    def parse_response(self, response):
-        return response.json()
-
-
-class AudioClassification:
-    def __init__(self, task: Task, model_id: str):
-        self.task = task
-        self.model_id = model_id
-
-    @property
-    def inference_inputs(self):
-        return encode_audio(self.task.args["audio"])
-
-    def parse_response(self, response):
-        return response.json()
-
-
-class TextGeneration:
-    def __init__(self, task: Task, model_id: str):
-        self.task = task
-        self.model_id = model_id
-
-    @property
-    def inference_inputs(self):
-        return self.task.args["text"]
-
-    def parse_response(self, response):
-        return response.json()
-
-
 class CXRToReportGeneration:
-    def __init__(self, task: str, model_id: str):
+    def __init__(self, task: str, model_id: str, input_args: Any):
         self.task = task
         self.model_id = model_id
+        self.input_args = input_args
 
     @property
     def inference_inputs(self):
-        if "text" in self.task.args:
-            instruction = self.task.args["text"]
-        else:
-            instruction = "Generate free-text radiology reports for the entered chest X-ray images."
-        img_url = self.task.args["image"]
-        print(img_url)
-        img_array = self.encode_image_from_url(img_url)
-        data = {
-            "inputs": {
-                "instruction": instruction,
-                "input": img_array,
-            }
-        }
+        data = self.convert_image_urls(self.input_args)
+        return {"inputs": data}
+
+    def convert_image_urls(self, data):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                data[key] = self.convert_image_urls(value)
+        elif isinstance(data, list):
+            data = [self.convert_image_urls(item) for item in data]
+        elif isinstance(data, str) and data.startswith("https://chatmedi-s3.s3.ap-northeast-2.amazonaws.com"):
+            data = self.encode_image_from_url(data)
+
         return data
 
     def encode_image_from_url(self, url):
@@ -465,28 +126,14 @@ class CXRToReportGeneration:
 
 
 class ReportToCXRGeneration:
-    def __init__(self, task: str, model_id: str):
+    def __init__(self, task: str, model_id: str, input_args: Any):
         self.task = task
         self.model_id = model_id
+        self.input_args = input_args
 
     @property
     def inference_inputs(self):
-        model_input_format = get_model_input_format(self.model_id)
-        if isinstance(model_input_format, dict):
-            data = {
-                "inputs": {
-                    "instruction": model_input_format["instruction"],
-                    "input": self.task.args["text"]
-                }
-            }
-        elif model_input_format == "text":
-            data = {
-                "inputs": self.task.args["text"]
-            }
-        else:
-            raise ValueError("Unknown input format for model")
-
-        return data
+        return {"inputs": self.input_args}
 
     def save_image_s3(self, image: Image) -> str:
         buffered = BytesIO()
@@ -518,58 +165,15 @@ class ReportToCXRGeneration:
                 "image": file_location,
                 "text": ""
             }}
-    
-
-# class CXRVQA:
-#     def __init__(self, task: Task):
-#         self.task = task
-
-#     @property
-#     def inference_inputs(self):
-#         img_data = encode_image(self.task.args["image"])
-#         img_base64 = base64.b64encode(img_data).decode("utf-8")
-#         data = {
-#             "inputs": {
-#                 "instruction": self.task.args["text"],
-#                 "input": img_base64,
-#             }
-#         }
-#         return data
-
-#     def save_image_locally(self, image: Image) -> str:
-#         buffered = BytesIO()
-#         image.save(buffered, format="PNG")
-#         buffered.seek(0)
-#         upload_file = UploadFile(filename="generated_image.png", file=buffered)
-#         file_location = upload_to_s3(upload_file, content_type="image/png")
-#         return file_location
-
-#     def parse_response(self, response):
-#         rgb_data = response["result_img"]
-#         rgb_array = np.array(rgb_data, dtype=np.uint8)
-#         image = Image.fromarray(rgb_array, 'RGB')
-
-#         # 로컬에 이미지 저장
-#         file_location = self.save_image_locally(image)
-
-#         return {
-#             "result": {
-#                 "image": file_location,
-#                 "text": response["result_text"]
-#             }
-#         }
-
 
 HUGGINGFACE_TASKS = {
     "cxr-to-report-generation": CXRToReportGeneration,
     "report-to-cxr-generation": ReportToCXRGeneration,
-    # "cxr-visual-qestion-answering": CXRVQA,
 }
 
-
-def create_huggingface_task(task: str, model_id: str):
+def create_huggingface_task(task: str, model_id: str, input_args: Any):
     if task in HUGGINGFACE_TASKS:
-        return HUGGINGFACE_TASKS[task](task, model_id)
+        return HUGGINGFACE_TASKS[task](task, model_id, input_args)
     else:
         raise NotImplementedError(f"Task {task} not supported")
 
